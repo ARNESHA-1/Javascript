1d73a8093eaedb3a081826aa144f3ff67c3da817
ARNESHA-1 <arnesha3009@gmail.com>
2025-07-21 19:10:26 +0000

    config commit change

---
+++ 01_basics/operation_conversion.js
@@ -0,0 +1,9 @@
+let someNum = 54
+let someString = String(someNum)
+console.log(someString)
+console.log(typeof someString)
+//***********  operations  *************//
+
+let val = 33
+let negVal = -val
+console.log(negVal)
