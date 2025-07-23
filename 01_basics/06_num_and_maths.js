b0a6333195ccb56f48fa106eef275bd0fde94538
ARNESHA-1 <arnesha3009@gmail.com>
2025-07-23 13:46:03 +0000

    Nums and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,15 @@
+
+const val = 90
+const num = new Number(5)
+
+console.log(Math)    //Object [Math] {}
+console.log(Math.abs(-2))
+console.log(Math.round(4.8))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.3))
+
+console.log(Math.min(4,2,3,0))
+console.log(Math.max(9,8,0,1))
+const max = 5
+const min = 1
+console.log(Math.random()*(max-min)+ min)
