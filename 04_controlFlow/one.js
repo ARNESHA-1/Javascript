4bf2946d48a0b12025edc21d1ac38521aef7ac4f
ARNESHA-1 <arnesha3009@gmail.com>
2025-07-28 18:19:36 +0000

    Control Flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,10 @@
+ //if
+ const isLoggedIn = true
+
+ if(2 == "2"){
+    console.log("Executed 1")
+    if(2 === "2"){
+        console.log("Exexuted 2")
+    }
+ }
+ // < , >, >=, <=, ==, ===, !=, !==
---
+++ 04_controlFlow/two.js
@@ -0,0 +1,35 @@
+ //switch case
+
+//  switch(key){
+//     case value1:
+//         break;
+//     case value2:
+//         break;
+//     default:
+//         break;
+//  }
+
+let month = 8;
+switch(month){
+    case 3:
+        console.log("No birthdays")
+        break;
+    case 4:
+        console.log("No birthdays")
+        break;
+    case 5:
+        console.log("No birthdays")
+        break;
+    case 8:
+        console.log("HBD!!!")
+        break;
+    case 9:
+        console.log("No birthdays")
+        break;
+    case 10:
+        console.log("No birthdays")
+        break;
+    default:
+        console.log("Bye")
+        break;
+ }
